5f575c663d86c25f4ad710451e216e48b250d4d0
Ashwin Kumar <107858939+Ash6323@users.noreply.github.com>
2023-04-13 15:23:21 +0530

    Did Changes
    Used props for Component Interaction using Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,24 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-// import ModalDialog from './components/ModalDialog'
+import ModalDialog from './components/ModalDialog'
 import { Modal, Button } from "react-bootstrap";
-import { Step, Stepper } from "react-form-stepper";
 
-function App() {
+const App: React.FC = () => {
   interface Item {
     id: number;
-    itemName: string;
-    itemDescription: string;
-    itemPrice: number;
-    itemQuantity: number;
+    item_Name: string;
+    item_Description: string;
+    item_Price: number;
+    item_Quantity: number;
   }
-
-  const [initItemName, setitemName] = useState<string>("");
-  const [initItemDescription, setitemDescription] = useState<string>("");
-  const [initItemPrice, setitemPrice] = useState<number>(Number);
-  const [initItemQuantity, setitemQuantity] = useState<number>(Number);
-
-  const [initialItem, setInitialItem] = useState<Item>
-  ({
-    id: Date.now(),
-    itemName: initItemName,
-    itemDescription: initItemDescription,
-    itemPrice: initItemPrice,
-    itemQuantity: initItemQuantity,
-  });
 
   const [itemList, setItemList] = useState<Item[]>([]);
 
-  const [step, setNewStep] = useState(1);
-  const nextStep = () => { setNewStep(step + 1); };
-  const prevStep = () => { setNewStep(step - 1); };
-
-  const [isShow, invokeModal] = React.useState(false);
+  const [isShow, invokeModal] = useState(false);
+  
   const closeModal = () => invokeModal(false);
-  const showModal = () => invokeModal(true);
-
-  const handleChange_ItemName = (e: ChangeEvent<HTMLInputElement>) => {
-    setitemName(e.target.value);
-  };
-  const handleChange_ItemDescription = (e: ChangeEvent<HTMLInputElement>) => {
-    setitemDescription(e.target.value);
-  };
-  const handleChange_ItemPrice = (e: ChangeEvent<HTMLInputElement>) => {
-    setitemPrice(e.target.valueAsNumber);
-  };
-  const handleChange_ItemQuantity = (e: ChangeEvent<HTMLInputElement>) => {
-    setitemQuantity(e.target.valueAsNumber);
-  };
+  const handleAddClick = () => {invokeModal(true);};
 
   const [count, setCount] = useState(0);
   function SortItemList() 
@@ -57,12 +26,12 @@ function App() {
     setCount(count + 1);
     if(count%2 == 1)
     {
-      itemList.sort((a, b) => a.itemName.localeCompare(b.itemName));
+      itemList.sort((a, b) => a.item_Name.localeCompare(b.item_Name));
       setItemList([...itemList]);
     }
     else if(count%2 == 0)
     {
-      itemList.sort((a, b) => b.itemName.localeCompare(a.itemName));
+      itemList.sort((a, b) => b.item_Name.localeCompare(a.item_Name));
       setItemList([...itemList]);
     }
   }
@@ -70,7 +39,7 @@ function App() {
   const [search, setSearch] = useState('');
   const filteredItems = {
     list: itemList.filter((item) =>
-      item.itemName.toLowerCase().includes(search.toLowerCase())
+      item.item_Name.toLowerCase().includes(search.toLowerCase())
     ),
   };
 
@@ -78,23 +47,18 @@ function App() {
     setSearch(event.target.value);
   };
 
-  // const [isDisabled, setDisabled] = useState(false);
-  // const ItemNameValidation = () => {
-  //   if (initItemName.length >= 30 || initItemDescription.length <= 100 || initItemName.length <= 0)
-  //     setDisabled(true);
-  // }
-
-  function AddItem() 
+  const AddItem = (itemName: string, itemDescription: string, itemPrice: number, itemQuantity: number) =>
   {
     const newItem: Item = 
     {
       id: Date.now(),
-      itemName: initItemName,
-      itemDescription: initItemDescription,
-      itemPrice: initItemPrice,
-      itemQuantity: initItemQuantity,
+      item_Name: itemName,
+      item_Description: itemDescription,
+      item_Price: itemPrice,
+      item_Quantity: itemQuantity,
     };
     setItemList([...itemList, newItem]);
+    console.log("App.tsx",itemList);
   }
   const RemoveItem = (id: number) => {
     const oldValues = itemList.filter((item) => item.id !== id);
@@ -120,7 +84,7 @@ function App() {
               <datalist id="items-list">
                 {itemList.map((item) => (
                   <div key={item.id}>
-                    <option value={item.itemName}></option>
+                    <option value={item.item_Name}></option>
                   </div>
                   ))}
               </datalist>
@@ -128,7 +92,7 @@ function App() {
             <Button
               className="add-item-btn"
               variant="success"
-              onClick={showModal}> 
+              onClick={handleAddClick}> 
               Add Item
             </Button>
             <Button
@@ -137,84 +101,11 @@ function App() {
               onClick={SortItemList}>
               Sort Items
             </Button>
-            <Modal show={isShow}>
-              <Modal.Header closeButton onClick={closeModal}>
-              <Modal.Title>Enter New Item Details</Modal.Title>
-                  <Stepper className="my-stepper"
-                    steps={[{ label: "Item Details" },{ label: "Item Billing" }]}
-                    activeStep={step}
-                  />
-              </Modal.Header>
-              
-              {step === 1 && (
-                <> 
-                <Modal.Body>
-                  <div>
-                  <label>Enter Item Name:</label>
-                  <input
-                    className="input-item-details"
-                    type="text"
-                    placeholder="New Item Name"
-                    onChange={handleChange_ItemName}
-                  />
-                  </div>
-                  <div>
-                  <label>Enter Item Description:</label>
-                  <input
-                    className="input-item-details"
-                    type="textarea"
-                    placeholder="New Item Description"
-                    onChange={handleChange_ItemDescription}
-                  />
-                  </div>
-                </Modal.Body>
-                <Modal.Footer>            
-                  <Button variant="danger" onClick={closeModal}>
-                    Close
-                  </Button>
-                  <Button variant="dark" 
-                          onClick={nextStep}
-                          disabled={initItemName.length >= 30 || initItemDescription.length <= 100 || initItemName.length <= 0}>
-                    Next
-                  </Button>
-                </Modal.Footer>
-                </>
-              )}
-              {step === 2 && (
-              <>
-                <Modal.Body>
-                  <div>
-                  <label>Enter Item Price:</label>
-                  <input
-                    className="input-item-details"
-                    type="number"
-                    placeholder="New Item Price"
-                    onChange={handleChange_ItemPrice}
-                  />
-                  </div>
-                  <div>
-                  <label>Enter Item Quantity:</label>
-                  <input
-                    className="input-item-details"
-                    type="number"
-                    placeholder="New Item Quantity"
-                    onChange={handleChange_ItemQuantity}
-                  />
-                  </div>
-                </Modal.Body>
-                <Modal.Footer>            
-                  <Button variant="danger" onClick={prevStep}>
-                    Back
-                  </Button>
-                  <Button variant="dark" 
-                          onClick={AddItem}
-                          disabled={initItemPrice == 0 || initItemQuantity == 0}>
-                    Add Item
-                  </Button>
-                </Modal.Footer>
-              </>
-              )}
+            <Modal show={isShow} onHide={() => invokeModal(false)}>
+
+            <ModalDialog propItem={AddItem}/>
             </Modal>
+
           </div>
 
           <hr></hr>
@@ -231,10 +122,10 @@ function App() {
               <tbody>
                 {filteredItems.list.map((item) => (
                   <tr key={item.id}>
-                    <td>{item.itemName}</td>
-                    <td>{item.itemDescription}</td>
-                    <td>{item.itemPrice}</td>
-                    <td>{item.itemQuantity}</td>
+                    <td>{item.item_Name}</td>
+                    <td>{item.item_Description}</td>
+                    <td>{item.item_Price}</td>
+                    <td>{item.item_Quantity}</td>
                   </tr>
                 ))}
               </tbody>
